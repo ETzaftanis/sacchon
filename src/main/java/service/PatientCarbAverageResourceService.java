51a0fbd3f90b0b98d03d4ae70e611e4e8b686952
DESKTOP-EPI2VVV\Manos <tzaftanis@gmail.com>
2021-05-12 13:25:43 +0300

    PatientCarbAverageResourceService implemented

--- src/main/java/service/PatientCarbAverageResourceService.java
+++ src/main/java/service/PatientCarbAverageResourceService.java
@@ -1,4 +1,25 @@
 package service;
 
+import jpaUtil.JpaUtil;
+import repository.PatientRepository;
+import resource.ResourceUtils;
+
+import javax.persistence.EntityManager;
+import java.util.Date;
+
 public class PatientCarbAverageResourceService {
+
+    public static Double getAverageCarb(long id, String start, String end) {
+
+        Date dateStart = ResourceUtils.stringToDate(start, -1);
+        Date dateEnd = ResourceUtils.stringToDate(end, 1);
+
+        EntityManager em = JpaUtil.getEntityManager();
+
+        PatientRepository patientRepository = new PatientRepository(em);
+        Double carb = patientRepository.getCarbAverage(id, dateStart, dateEnd);
+
+        em.close();
+        return carb;
+    }
 }
